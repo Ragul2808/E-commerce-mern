0a3ecc69654c61380637ea0b46d6495f2ff76e62
ragul <ragulraja2808@gmail.com>
2025-03-01 08:08:44 +0530

    refactor main.jsx for improved import structure and consistency

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,13 @@
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
-import { store } from './store';
-import { Provider } from 'react-redux'; // Fix: Capitalized "Provider"
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <Provider store={store}> {/* Fix: Capitalized "Provider" */}
+    <Provider store={store}>
       <App />
     </Provider>
   </BrowserRouter>
