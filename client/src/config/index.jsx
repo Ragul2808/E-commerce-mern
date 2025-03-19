35c4dda4e5ca259c6067430feda65eb665b31626
ragul <ragulraja2808@gmail.com>
2025-03-20 05:15:57 +0530

    feat(RegisterForm): add configuration for register form controls

---
+++ client/src/config/index.jsx
@@ -0,0 +1,25 @@
+export const registerFormControls = [
+    {
+        name:'username',
+        label:'Username',
+        placeholder:'Enter your username',
+        commentType: 'input',
+        type:'text',
+    },
+    {
+        name:'email',
+        label:'Email',
+        placeholder:'Enter your Email',
+        commentType: 'input',
+        type:'email',
+    },
+    {
+        name:'password',
+        label:'Password',
+        placeholder:'Enter your password',
+        commentType: 'input',
+        type:'password',
+    }
+
+    
+]
